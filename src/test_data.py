5b6e4e042babaeafb2d5976208ee79ddc3f95f16
Fabio Niephaus <fniephaus@users.noreply.github.com>
2015-01-15 10:47:38 +0100

    Add some tests

---
+++ src/test_data.py
@@ -0,0 +1,116 @@
+def get_initial():
+    return {
+        u'status': 1,
+        u'error': None,
+        u'complete': 1,
+        u'since': 1,
+        u'list': {
+            u'1111': {
+                u'status': u'0',
+                u'is_index': u'1',
+                u'time_updated': u'54321',
+                u'time_favorited': u'0',
+                u'excerpt': u'abc123',
+                u'has_image': u'1',
+                u'favorite': u'0',
+                u'given_title': u'',
+                u'resolved_url': u'',
+                u'is_article': u'0',
+                u'item_id': u'1111',
+                u'time_added': u'54321',
+                u'resolved_id': u'1111',
+                u'given_url': u'',
+                u'resolved_title': u''
+            },
+            u'2222': {
+                u'status': u'0',
+                u'is_index': u'0',
+                u'time_updated': u'1396054841',
+                u'time_favorited': u'0',
+                u'excerpt': u'abc',
+                u'has_image': u'0',
+                u'favorite': u'0',
+                u'given_title': u'',
+                u'resolved_url': u'',
+                u'is_article': u'1',
+                u'item_id': u'2222',
+                u'time_added': u'1396054841',
+                u'resolved_id': u'2222',
+                u'given_url': u'',
+                u'resolved_title': u''
+            },
+            u'3333': {
+                u'videos': {},
+                u'is_article': u'0',
+                u'excerpt': u'text2',
+                u'time_added': u'1411528927',
+                u'status': u'0',
+                u'resolved_url': u'',
+                u'item_id': u'3333',
+                u'resolved_id': u'722186783',
+                u'given_url': u'',
+                u'resolved_title': u'resolvedtitle',
+                u'is_index': u'0',
+                u'time_updated': u'1411528927',
+                u'time_favorited': u'0',
+                u'favorite': u'0',
+                u'given_title': u'title321',
+                u'has_image': u'1'
+            }
+        }
+    }
+
+def get_delta(since):
+    return {
+        u'status': 1,
+        u'error': None,
+        u'complete': 0,
+        u'since': since + 10,
+        u'list': {
+            u'4444': {
+                u'status': u'0',
+                u'is_index': u'0',
+                u'time_updated': u'1420711247',
+                u'time_favorited': u'0',
+                u'excerpt': u'',
+                u'has_image': u'0',
+                u'favorite': u'0',
+                u'given_title': u'title123',
+                u'resolved_url': u'',
+                u'is_article': u'0',
+                u'item_id': u'4444',
+                u'time_added': u'1420711246',
+                u'resolved_id': u'4444',
+                u'given_url': u'',
+                u'resolved_title': u'',
+                u'tags': {u'alfred': {u'item_id': u'4444',
+                u'tag': u'alfred'}}},
+
+            u'1111': {
+                u'status': u'1',
+                u'is_index': u'0',
+                u'time_updated': u'1420892898',
+                u'time_favorited': u'0',
+                u'excerpt': u"ec",
+                u'has_image': u'1',
+                u'favorite': u'0',
+                u'given_title': u'',
+                u'resolved_url': u'',
+                u'is_article': u'1',
+                u'item_id': u'1111',
+                u'time_added': u'1420828162',
+                u'resolved_id': u'1111',
+                u'given_url': u'',
+                u'resolved_title': u''
+            }
+        }
+    }
+
+def get_end(since):
+    return {
+        u'status': 0,
+        u'error': None,
+        u'complete': 0,
+        u'since': since + 10,
+        u'list': {}
+    }
--- src/tests.py
+++ src/tests.py
@@ -1,10 +1,18 @@
 import unittest
 
+import test_data
+import pocket
+from pocket_api import AuthException, PocketException
+from workflow import PasswordNotFound
 import pocket_refresh
+import pocket_refresh as pocket_refresh_backup
+
+CachedData = {}
+Passwords = {}
 
 class PocketTestCase(unittest.TestCase):
 
-   def test_sync_data(self):
+    def test_sync_data(self):
         links = {}
 
         data = {}
@@ -27,6 +35,83 @@ class PocketTestCase(unittest.TestCase):
         self.assertTrue('1234' in links)
         self.assertFalse('5678' in links)
 
+    def test_exception_handling(self):
+        def get(self, state=None, favorite=None, tag=None, contentType=None,
+                sort=None, detailType=None, search=None, domain=None, since=None,
+                count=None, offset=None):
+            raise AuthException
+        pocket_refresh.Pocket.get = get
+        pocket_refresh.main()
+
+        def get(self, state=None, favorite=None, tag=None, contentType=None,
+                sort=None, detailType=None, search=None, domain=None, since=None,
+                count=None, offset=None):
+            raise PocketException
+        pocket_refresh.Pocket.get = get
+        pocket_refresh.main()
+
+        def get_password(self, *args):
+            raise PasswordNotFound
+        pocket_refresh.Workflow.get_password = get_password
+        pocket_refresh.main()
+
+    def test_refresh(self):
+        self.monkeypatch_refresh()
+        pocket_refresh.main()
+        self.assertTrue('1111' in CachedData['pocket_list'])
+        self.assertTrue('2222' in CachedData['pocket_list'])
+        self.assertTrue('3333' in CachedData['pocket_list'])
+        self.assertEquals(len(CachedData['pocket_list']), 3)
+        
+        pocket_refresh.main()
+        self.assertTrue('1111' not in CachedData['pocket_list'])
+        self.assertTrue('2222' in CachedData['pocket_list'])
+        self.assertTrue('3333' in CachedData['pocket_list'])
+        self.assertTrue('4444' in CachedData['pocket_list'])
+
+    def monkeypatch_refresh(self):
+        def get(
+                self, state=None, favorite=None, tag=None, contentType=None,
+                sort=None, detailType=None, search=None, domain=None, since=None,
+                count=None, offset=None):
+            if offset == 0:
+                if CachedData == {}:
+                    return [test_data.get_initial()]
+                else:
+                    return [test_data.get_delta(since)]
+            return [test_data.get_end(since)]
+
+        pocket_refresh.Pocket.get = get
+
+        pocket_refresh.Workflow.get_password = lambda x, y: x
+
+        def cached_data(self, key, max_age=None):
+            return CachedData.get(key)
+        pocket_refresh.Workflow.cached_data = cached_data
+
+        def cache_data(self, key, data):
+            CachedData[key] = data
+        pocket_refresh.Workflow.cache_data = cache_data
+
+    def setUp(self):
+        pocket_refresh = pocket_refresh_backup
+        CachedData = {}
+        Passwords = {}
+
+        def cached_data(self, key, max_age=None):
+            pass
+        def cache_data(self, key, data):
+            pass
+        pocket_refresh.Workflow.cached_data = cached_data
+        pocket_refresh.Workflow.cache_data = cache_data
+
+        def get_password(self, key):
+            return Passwords.get(key)
+        pocket_refresh.Workflow.get_password = get_password
+        def delete_password(self, key):
+            if key in Passwords:
+                del Passwords[key]
+        pocket_refresh.Workflow.delete_password = delete_password
 
 
 if __name__ == "__main__":

90021333db8bff6a66f788f1188318279cbf38ba
k <rockkoca@gmail.com>
2018-03-21 21:34:44 -0700

    full text search with whoosh skip last incomplete keyword if no results case in-sensitive search finalized

--- src/test_fulltext_local.py
+++ src/test_fulltext_local.py
@@ -42,6 +42,22 @@ class PocketFullTextLocalTestCase(unittest.TestCase):
             for keyword in keywords:
                 self.assertTrue(url not in {item['url'] for item in FullText.get_instance().search(keyword)})
 
+    def test_delete_docs(self):
+        cases = {
+            u'https://askubuntu.com/questions/798516/what-tool-adds-unlimited-clipboard-and-searchable-retrieval':
+                [u'ALFRED Powerpack', u'clipboard manager'],
+            u'https://www.zhihu.com/question/20186320':
+                [u'Pocket', u'有自己的图片服务器'],
+            u'https://www.quora.com/What-does-Andrew-Ng-think-about-Deep-Learning':
+                [u'algorithms that can take advantage', u'AI made tremendous'],
+
+        }
+        for url, keywords in cases.items():
+            FullText.get_instance().add_page(url)
+            FullText.get_instance().del_page(url)
+            for keyword in keywords:
+                self.assertTrue(url not in {item['url'] for item in FullText.get_instance().search(keyword)})
+
 
 if __name__ == "__main__":
     unittest.main()
